d00fe4f160316e105868ccbfe6a8ad2b7760e026
Amit-Bongale <amitbongale14@gmail.com>
2026-04-10 16:24:34 +0530

    Refactor HTML, CSS, and JavaScript for improved styling and functionality

--- js/swiper.js
+++ js/swiper.js
@@ -65,13 +65,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
     activate(0);
 });
+
+
 // Continuous Marquee Swiper Loop Initialization
     document.addEventListener('DOMContentLoaded', () => {
         new Swiper('.marquee-swiper', {
             loop: true,
             slidesPerView: 'auto',
             spaceBetween: 0,
-            speed: 4000, 
+            speed: 3000, 
             autoplay: {
                 delay: 0, 
                 disableOnInteraction: false,
